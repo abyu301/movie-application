0d69e0226ab220f4413984c4048072c75514877d
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2026-02-09 23:39:13 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, {StrictMode} from 'react';
+
+const App = () => {
+    return (
+        <StrictMode>
+            <div>
+                <h1 className="text-3xl font-bold underline">
+                    Hello World!
+                </h1></div>
+        </StrictMode>
+    )
+}
+
+export default App
