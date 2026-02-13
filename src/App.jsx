19373f864c72001d435f6ccdf9a574bf4cfde31c
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2026-02-12 19:26:16 -0700

    feat: integrate TMDB API and implement movie fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./assets/background.png";
 import Banner from "./assets/banner.png";
 import Search from "./components/Search.jsx";
 
+
+const API_BASE_URL = "https://api.themoviedb.org/3/";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: "GET",
+    headers:{
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
+
 const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const fetchMovies = async () => {
+        try {
+            const endPoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+            const response = await fetch(endPoint, API_OPTIONS);
+
+            if (!response.ok) {
+                throw new Error("Could not find movie?");
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+        } catch (error) {
+            console.log(`Fetch movies error: ${error}`);
+            setErrorMessage('Error fetching movies. Please try again later.');
+        }
+    }
+
+
+    useEffect(() => {
+        fetchMovies();
+
+    }, []);
     return (
         <header className="min-h-screen text-white overflow-x-hidden">
             <section className="relative min-h-screen flex items-center justify-center px-4 overflow-hidden">
@@ -28,7 +68,15 @@ const App = () => {
                     <h1 className="text-white">{searchTerm}</h1>
                 </div>
 
+
+                <div className="all-movies relative flex items-center justify-center px-4 overflow-hidden">
+                    <h2>All Movies</h2>
+
+                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                </div>
             </section>
+
+
         </header>
 
     );
