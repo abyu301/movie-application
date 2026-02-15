66aabe8a7a829adf3c64dc640c36c3791a16d993
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2026-02-14 17:05:54 -0700

    feat: integrate Appwrite database to track search count with proper configuration and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import { useDebounce} from "react-use";
+import {updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -44,6 +45,11 @@ const App = () => {
 
             const data = await response.json();
             setMoviesList(data.results || []);
+
+            if (query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0]);
+            }
+
         } catch (error) {
             console.log(error);
             setErrorMessage("Error fetching movies. Please try again later.");
